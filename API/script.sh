f375bd0c9a615acc88ab02000f219d082405cfcf
medkhalil <medkha.bt@gmail.com>
2025-12-06 02:18:32 +0100

    ci: rm old svgs

--- API/script.sh
+++ API/script.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+rm outputs/* || true
 cd ../Core/build && ./2SATEquivalenceReduction -f ../../API/$1 -a gws -g
 cp graphs/inputs/svg/${1%.*}* ../../API/outputs
 cd ../../API/outputs && rm result.svg || true
